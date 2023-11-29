7c890c17439695deb8518182b5b6c82a61290a79
unknown <dang81.djg@gmail.com>
2023-11-28 18:58:48 -0600

    initial hello world

---
+++ djg-cracus/src/index.js
@@ -0,0 +1,18 @@
+// import React from 'react'
+// import * as ReactDOM from 'react-dom/client'
+
+// import App from './components/App'
+
+// const container = document.getElementById('root');
+// const root = ReactDOM.createRoot(container);
+
+// root.render()
+
+// ReactDOM.createRoot(<App />, document.querySelectorAll('#root'))
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './components/App'
+
+ReactDOM.render(<App />, document.querySelector('#root'))
